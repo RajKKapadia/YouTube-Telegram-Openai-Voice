a097e7b1529dbce02978924e4ec5332da6c8164f
RajKKapadia <raajforyou@gmail.com>
2023-07-28 19:18:54 +0530

    added try catch

--- app.py
+++ app.py
@@ -16,30 +16,40 @@ def home():
 
 @router.post('/telegram')
 async def telegram(request: Request):
-    body = await request.json()
-    print(body)
-    sender_id = body['message']['from']['id']
-    if 'voice' in body['message'].keys():
-        file_id = body['message']['voice']['file_id']
-        file_path = get_file_path(file_id)
-        if file_path['status'] == 1:
-            local_file_path = save_file_and_get_local_path(file_path['file_path'])
-            if local_file_path['status'] == 1:
-                transcript = transcript_audio(local_file_path['local_file_path'], local_file_path['file_id'])
-                if transcript['status'] == 1:
-                    query = chat_completion(transcript['transcript'])
-    else:
-        query = body['message']['text']
-    response = chat_completion(query)
-    send_message(sender_id, response)
-    return 'OK', 200
+    try:
+        body = await request.json()
+        print(body)
+        sender_id = body['message']['from']['id']
+        if 'voice' in body['message'].keys():
+            file_id = body['message']['voice']['file_id']
+            file_path = get_file_path(file_id)
+            if file_path['status'] == 1:
+                local_file_path = save_file_and_get_local_path(file_path['file_path'])
+                if local_file_path['status'] == 1:
+                    transcript = transcript_audio(local_file_path['local_file_path'], local_file_path['file_id'])
+                    if transcript['status'] == 1:
+                        query = chat_completion(transcript['transcript'])
+        else:
+            query = body['message']['text']
+        response = chat_completion(query)
+        send_message(sender_id, response)
+        return 'OK', 200
+    except Exception as e:
+        print('Error at telegram...')
+        print(e)
+        return 'OK', 200
 
 
 @router.route('/set-telegram-webhook', methods=['POST'])
 async def set_telegram_webhook(request: Request):
-    body = await request.json()
-    flag = set_webhook(body['url'], body['secret_token'])
-    if flag:
-        return 'OK', 200
-    else:
+    try:
+        body = await request.json()
+        flag = set_webhook(body['url'], body['secret_token'])
+        if flag:
+            return 'OK', 200
+        else:
+            return 'BAD REQUEST', 400
+    except Exception as e:
+        print('Error at set_telegram_webhook...')
+        print(e)
         return 'BAD REQUEST', 400
