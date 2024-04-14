babff44f7a9fa8a50aaeebd2ddf535d6f29d45fb
RajKKapadia <raajforyou@gmail.com>
2024-04-14 14:47:52 +0530

    added text to speech

--- openai_apis.py
+++ openai_apis.py
@@ -1,12 +1,14 @@
 import os
 import tempfile
+import uuid
 
-import openai
+from openai import OpenAI
 import soundfile as sf
-from dotenv import find_dotenv, load_dotenv
-load_dotenv(find_dotenv())
+import config
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(
+    api_key=config.OPENAI_API_KEY
+)
 
 OUTPUT_DIR = os.path.join(
     tempfile.gettempdir(),
@@ -28,11 +30,14 @@ def transcript_audio(ogg_file_path: str, file_id: str) -> dict:
         audio_file = open(mp3_file_path, 'rb')
         os.unlink(ogg_file_path)
         os.unlink(mp3_file_path)
-        transcript = openai.Audio.transcribe(
-            'whisper-1', audio_file)
+        transcript = client.audio.transcriptions.create(
+            model='whisper-1',
+            file=audio_file,
+            response_format="text"
+        )
         return {
             'status': 1,
-            'transcript': transcript['text']
+            'transcript': transcript
         }
     except Exception as e:
         print('Error at transcript_audio...')
@@ -45,15 +50,34 @@ def transcript_audio(ogg_file_path: str, file_id: str) -> dict:
 
 def chat_completion(text: str) -> str:
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model='gpt-3.5-turbo',
             messages=[
                 {'role': 'system', 'content': 'You are a helpful assistant.'},
                 {'role': 'user', 'content': text}
             ]
         )
-        return response['choices'][0]['message']['content']
+        return response.choices[0].message.content
     except Exception as e:
         print('Error at chat_completion...')
         print(e)
         return 'We are facing an issue at this moment.'
+
+
+chat_completion('hi')
+
+
+def text_to_speech(text: str) -> tuple[str, str]:
+    file_name = f'{uuid.uuid1()}.{config.AUDIO_FILE_FORMAT}'
+    audio_file_path = os.path.join(
+        config.OUTPUT_DIR,
+        file_name
+    )
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text,
+        response_format=config.AUDIO_FILE_FORMAT
+    )
+    response.write_to_file(audio_file_path)
+    return audio_file_path, file_name
